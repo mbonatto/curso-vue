63e90e986b4f3da447f54fb092d044e9650a717b
Marcos Bonatto <mbonatto52@gmail.com>
2021-04-08 23:54:37 -0300

    Evento 4 + Desafio 2

---
+++ dom/desafio-02/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: ''
+    },
+    methods: {
+        showAlert: function() {
+            alert("Hello!")
+        },
+        /*save: function(event) {
+            this.valor = event.target.value;
+        }*/
+    }
+})
